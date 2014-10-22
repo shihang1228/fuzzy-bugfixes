c1dda1b701c433474a3d0671825852dc94f24693
shihang <study1228.sh@gmail.com>
2014-10-22 16:26:10 +0800

    在ContactController中自动装配ContactService，并调用其方法，并添加相应的测试

--- src/main/java/com/baldurtech/contact/ContactController.java
+++ src/main/java/com/baldurtech/contact/ContactController.java
@@ -2,6 +2,7 @@ package com.baldurtech.contact;
 
 import org.springframework.ui.Model;
 import org.springframework.stereotype.Controller;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import java.util.List;
@@ -10,27 +11,16 @@ import java.util.ArrayList;
 @Controller
 @RequestMapping("contact")
 public class ContactController {
-    @RequestMapping("list")
-    public String list(Model model) {    
-        model.addAttribute("contactList",findAll());
-        return "contact/list";
+    ContactService contactService;
+    
+    @Autowired
+    public ContactController(ContactService contactService) {
+        this.contactService = contactService;
     }
     
-    public List<Contact> findAll() {
-        List<Contact> contactList = new ArrayList<Contact>();
-        Contact contact = new Contact();
-        contact.setName("Shihang");
-        contact.setMobile("15235432994");
-        contact.setVpmn("652994");
-        contact.setHomeAddress("Taiyuan");
-        
-        Contact contact2 = new Contact();
-        contact2.setName("Xiaobai");
-        contact2.setMobile("18235100872");
-        contact2.setVpmn("65872");
-        contact2.setHomeAddress("Xiaodian");
-        contactList.add(contact);
-        contactList.add(contact2);
-        return contactList;
+    @RequestMapping("list")
+    public String list(Model model) {    
+        model.addAttribute("contactList",contactService.findAll());
+        return "contact/list";
     }
 }
--- src/test/java/com/baldurtech/contact/ContactControllerTest.java
+++ src/test/java/com/baldurtech/contact/ContactControllerTest.java
@@ -1,16 +1,41 @@
 package com.baldurtech.contact;
 
 import org.junit.Test;
+import org.junit.Before;
+import org.mockito.Mock;
+import org.mockito.InjectMocks;
+import org.mockito.MockitoAnnotations;
+import org.springframework.ui.Model;
 import com.baldurtech.config.WebAppConfigurationAware;
+import static org.mockito.Mockito.verify;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 
 public class ContactControllerTest extends WebAppConfigurationAware {
+    @Mock
+    ContactService contactService;
+    
+    @Mock
+    Model model;
+    
+    @InjectMocks
+    ContactController contactController;
+    
+    @Before
+    public void initMocks() {
+        MockitoAnnotations.initMocks(this);
+    }
     
     @Test
     public void 当URL为contact_list时应该访问list页面() throws Exception{
         mockMvc.perform(get("/contact/list"))
                .andExpect(view().name("contact/list"))
                .andExpect(model().attributeExists("contactList"));
+    }
+    
+    @Test
+    public void 在ContactController中的list方法中应该调用ContactService中的findAll方法() {
+        contactController.list(model);
+        verify(contactService).findAll();
     }
 }
