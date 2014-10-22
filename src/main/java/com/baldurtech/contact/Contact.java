39487dcb0da7dc13d499f3528711617ac5846b19
shihang <study1228.sh@gmail.com>
2014-10-22 17:14:31 +0800

    实现了findAll从数据库中查询contact的功能，并在页面中显示出来

--- src/main/java/com/baldurtech/contact/Contact.java
+++ src/main/java/com/baldurtech/contact/Contact.java
@@ -1,8 +1,17 @@
 package com.baldurtech.contact;
 
+import javax.persistence.*;
+
+@Entity
+@Table(name="contact")
+@NamedQuery(name = Contact.FIND_ALL, query = "from Contact")
 public class Contact
 {
+    public static final String FIND_ALL = "Contact.findAll";
+    @Id
+    @GeneratedValue
     private Long id;
+    
     private String name;
     private String mobile;
     private String vpmn;
--- src/main/java/com/baldurtech/contact/ContactRepository.java
+++ src/main/java/com/baldurtech/contact/ContactRepository.java
@@ -2,25 +2,22 @@ package com.baldurtech.contact;
 
 import java.util.List;
 import java.util.ArrayList;
+import javax.persistence.*;
 import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
 
 @Repository
+@Transactional
 public class ContactRepository {
+    @PersistenceContext
+    EntityManager entityManager;
+       
     public List<Contact> findAll() {
-        List<Contact> contactList = new ArrayList<Contact>();
-        Contact contact = new Contact();
-        contact.setName("Shihang");
-        contact.setMobile("15235432994");
-        contact.setVpmn("652994");
-        contact.setHomeAddress("Taiyuan");
-        
-        Contact contact2 = new Contact();
-        contact2.setName("Xiaobai");
-        contact2.setMobile("18235100872");
-        contact2.setVpmn("65872");
-        contact2.setHomeAddress("Xiaodian");
-        contactList.add(contact);
-        contactList.add(contact2);
-        return contactList;
+        try {        
+            return entityManager.createNamedQuery(Contact.FIND_ALL, Contact.class)
+                                .getResultList();
+        } catch(PersistenceException pe) {
+            return null;
+        }
     }
 }
