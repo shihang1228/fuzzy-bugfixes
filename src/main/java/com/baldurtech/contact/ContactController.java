6f17e6eb73c3ace8239c38d704331d5371db2a1a
shihang <study1228.sh@gmail.com>
2014-10-22 15:39:40 +0800

    在页面中显示一个空的集合

--- src/main/java/com/baldurtech/contact/ContactController.java
+++ src/main/java/com/baldurtech/contact/ContactController.java
@@ -1,13 +1,19 @@
 package com.baldurtech.contact;
 
+import org.springframework.ui.Model;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.util.List;
+import java.util.ArrayList;
 
 @Controller
 @RequestMapping("contact")
 public class ContactController {
     @RequestMapping("list")
-    public String list() {
+    public String list(Model model) {
+        List<Contact> contactList = new ArrayList<Contact>();
+        model.addAttribute("contactList", contactList);
         return "contact/list";
     }
 }
